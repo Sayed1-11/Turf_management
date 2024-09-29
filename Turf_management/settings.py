74fa2e0196db7200fe667496ed032e13be45a7a0
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-09-29 23:45:18 +0600

    Update settings.py

--- Turf_management/settings.py
+++ Turf_management/settings.py
@@ -109,7 +109,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('red-crq584u8ii6s73cpl5r0', 6379)],
         },
     },
 }
